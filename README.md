59d8072b09a18a2951a299da9734a7c960384b93
nyugoh <nyugoh@gmail.com>
2018-09-12 10:34:56 +0300

    Things to do

--- README.md
+++ README.md
@@ -5,3 +5,12 @@
 You are welcome to contribute to this project either as a sponsor or a developer.
 
 ***
+Remaining sections
+
+- [ ] Message listing to show kids name
+- [ ] Admin login and signup
+- [ ] Sponsors add and edit, upload profile not working
+- [ ] Style make over & add fonts
+- [ ] Sort updates according to the latest and display when it was added
+- [ ] Updates edit to include images upload
+- [ ] 
--- src/components/menus/Main.js
+++ src/components/menus/Main.js
@@ -6,7 +6,7 @@ const Main = () =>{
   return <Menu size='massive' borderless={true} color='blue' style={{border: 'none', borderRadius: 'none'}}>
     <div className="ui container">
       <Link to='/' className='item'>
-        <img alt='Fountain Hope Logo' src='/assets/images/logo.png' />
+        <img alt='Fountain Hope Logo' src='/assets/images/fhck.png' />
       </Link>
       <Link to='/kids' className='item' >Kids</Link>
       <Link to='/donate' className='item'>Donate</Link>
